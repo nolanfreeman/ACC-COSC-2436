5e01a63e125d40476ff9afad8782da2134a31561
nolan freeman abbott <nolan@freemanabbott.com>
2022-02-23 21:56:28 -0600

    More comments and rewrite sorting algorithm
    Idiotically used the selection sort algorithm as opposed to the bubble
    sort. Quickly realized this after trying to comment my code. +1 for
    proper documentation that is, I guess.

--- lab-1/ArrayBag.cpp
+++ lab-1/ArrayBag.cpp
@@ -5,17 +5,23 @@
 template<class ItemType>
 void ArrayBag<ItemType>::bubbleSort()
 {
-    int min{0};
+    // outer loop counter denotes the rightmost index that is sorted
     for (int low{0}; low < itemCount-1; ++low)
     {
-        min = low;
-        for (int cur{low+1}; cur < itemCount; ++cur)
-            if (items[cur] < items[min])
-                min = cur;
-
-        int temp{items[low]};
-        items[low] = items[min];
-        items[min] = temp;
+        // inner loop counter denotes the current
+        // index to be check with its neighbor
+        for (int cur{0}; cur < itemCount-low-1; ++cur)
+        {
+            // is the current index larger than its neighbor?
+            if(items[cur] > items[cur+1])
+            {
+                // perform swap of values using a temporary variable
+                // so to not lose the value of the current index
+                int temp = items[cur];
+                items[cur] = items[cur+1];
+                items[cur+1] = temp;
+            } // end if
+        } // end for
     } // end for
     isBagSorted = true;
 } // end bubbleSort
@@ -23,58 +29,77 @@ void ArrayBag<ItemType>::bubbleSort()
 template<class ItemType>
 int ArrayBag<ItemType>::binarySearchIterative(const ItemType& value) const
 {
-    bool valueFound{false};
+    bool valueFound{false};       // loop exit condition variable
 
-    int leftBound{0},
-        rightBound{itemCount-1},
-        midpoint{};
+    int leftBound{0},             // value of leftmost bound
+        rightBound{itemCount-1},  // value of rightmost bound
+        midpoint{};               // the midpoint between two bounds
 
     do { // while: !found, and there are still values to search through
+        // set midpoint to midpoint between two bounds
         midpoint = leftBound + (rightBound - leftBound) / 2;
 
         if (value == items[midpoint])
+            // midpoint is the value to find
             valueFound = true;
         else if (value > items[midpoint])
+            // value to find is right of midpoint
             leftBound = midpoint+1;
         else if (value < items[midpoint])
+            // value to find is left of midpoint
             rightBound = midpoint-1;
 
     } while (!valueFound && (leftBound <= rightBound));
 
+    // conditionally return -1 if value has not been found,
+    // because there are no more values to search through
     return valueFound ? midpoint : -1;
 } // end binarySearchIterative
 
 template<class ItemType>
 int ArrayBag<ItemType>::binarySearchRecursive(const ItemType& value) const
 {
-    int leftBound{0},
-        rightBound{itemCount-1},
-        midpoint{};
+    int leftBound{0},             // value of leftmost bound
+        rightBound{itemCount-1},  // value of rightmost bound
+        midpoint{};               // midpoint between two bounds
 
+    // call helper method to do all the dirty work
+    // leaving user with a clean client interface
     return binarySearchRecursiveHelper(value, leftBound, rightBound, midpoint);
 } // end binarySearchRecursive
 
 template<class ItemType>
 int ArrayBag<ItemType>::binarySearchRecursiveHelper(const ItemType& value, int& leftBound, int& rightBound, int& midpoint) const
 {
+    // set midpoint to midpoint between two bounds
     midpoint = leftBound + (rightBound - leftBound) / 2;
+
+    // loop exit condition variable
     bool valueFound{false};
 
+    // no more values to search through
     if (leftBound > rightBound)
     {
         midpoint = -1;
+        // found as in it is found to not exist in the list
         valueFound = true;
     }
+    // there are still values to search through
     else
     {
         if (value == items[midpoint])
+            // midpoint is the value to find
             valueFound = true;
         else if (value > items[midpoint])
+            // value to find is right of midpoint
             leftBound = midpoint+1;
         else if (value < items[midpoint])
+            // value to find is left of midpoint
             rightBound = midpoint-1;
     } // end if
 
+    // conditionally return the midpoint index if found (or not if -1)
+    // or recursively run the binary search again with the updated bounds
     return valueFound ? midpoint
                       : binarySearchRecursiveHelper(value, leftBound, rightBound, midpoint);
 } // end binarySearchRecursiveHelper
@@ -82,6 +107,8 @@ int ArrayBag<ItemType>::binarySearchRecursiveHelper(const ItemType& value, int& 
 template<class ItemType>
 bool ArrayBag<ItemType>::isSorted() const
 {
+    // return the ArrayBag variable storing the
+    // status of the bags sorted-ness
     return this->isBagSorted;
 } // end isSorted
 
